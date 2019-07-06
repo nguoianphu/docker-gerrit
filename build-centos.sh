d697c69f121e9ef943b91e8c58867187ae4c77d9
nguoianphu <nguoianphu@gmail.com>
2019-07-06 10:26:57 +0700

    Add Travis CI

---
+++ build-centos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -v
+
+cd centos/$CENTOS_VERSION/
+
+docker build -t docker-gerrit:$(git describe) .
+docker images
+docker run --name my-docker-gerrit -p 8080:8080 -p 29418:29418 docker-gerrit
+echo "Wait a few minutes until the Gerrit Code Review NNN ready message appears"
+docker ps -a
+docker exec -it my-docker-gerrit ps -ef
+docker logs my-docker-gerrit
+docker stop my-docker-gerrit
