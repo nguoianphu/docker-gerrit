8de26cb98729fcf6fb29b57ec54e64fb4bbd722e
nguoianphu <nguoianphu@gmail.com>
2019-07-07 08:50:41 +0700

    deploy docker images inside the build scripts

--- build-centos.sh
+++ build-centos.sh
@@ -5,8 +5,11 @@ cd centos/$CENTOS_VERSION/
 docker build -t ${DOCKER_USERNAME}/docker-gerrit .
 docker images
 docker run --name my-docker-gerrit -d -p 8080:8080 -p 29418:29418 ${DOCKER_USERNAME}/docker-gerrit
-echo "Wait a few minutes until the Gerrit Code Review NNN ready message appears"
 docker ps -a
 docker exec -it my-docker-gerrit ps -ef
 docker logs my-docker-gerrit
+curl http://localhost:8080
 docker stop my-docker-gerrit || true
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push "$DOCKER_USERNAME"/docker-gerrit
